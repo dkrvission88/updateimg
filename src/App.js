6b81eabecd2fa20f7a940eb97631c82b2f16a723
DKRVISSION <dkrvission88@gmail.com>
2024-02-24 15:59:08 +0530

    updatedimg

---
+++ src/App.js
@@ -0,0 +1,39 @@
+
+
+// import {BrowserRouter ,Routes,Route} from 'react-router-dom'
+// import Upcategary from './Component/Upcategary';
+// import ViewDetails from './Component/ViewDetails';
+// import Searcher from './Component/Searcher';
+
+// import EditSupplier from "./Profile/EditSupplier";
+import Supplier from "./Profile/Supplier";
+
+
+function App() {
+  return (
+
+    <>
+
+
+
+    {/* <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Upcategary/>}></Route>
+      <Route path='/ViewDetails' element={<ViewDetails/>}></Route>
+      <Route path='/Searcher' element={<Searcher/>}></Route>
+    </Routes>
+    </BrowserRouter> */}
+
+    {/* <ViewDetails/> */}
+    {/* <Upcategary/> */}
+
+    <Supplier/>
+    {/* <EditSupplier/> */}
+    
+    
+    </>
+   
+  );
+}
+
+export default App;
---
+++ src/Component/Upcategary.jsx
@@ -0,0 +1,296 @@
+import React, { useState } from 'react'
+
+import "./upcategory.css"
+import Send from '../Popup/Send'
+// import ViewDetails from './ViewDetails'
+
+
+import { useNavigate } from 'react-router-dom'
+
+function Upcategary() {
+    const [payment, setPayment] = useState(false)
+
+    const band = () => setPayment(false)
+    const Mymodel = () => {
+
+        setPayment(true);
+
+    }
+
+    const navigate = useNavigate();
+
+    const handleRedirect = () => {
+        // Redirect logic here
+        navigate('/ViewDetails'); // Use the navigate function from useNavigate
+    };
+
+    const navigate1 = useNavigate();
+
+    const handleRedirect1 = () => {
+        // Redirect logic here
+        navigate1('/Searcher'); // Use the navigate function from useNavigate/image/div0.jpg
+    };
+
+    // https://s3-alpha-sig.figma.com/img/c9d6/33d1/c15e5e78a1c3e43b8d1289ad583a5fd4?Expires=1708300800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ekRU8sGiEglmifzKQ32aotspCsyCZQodEIUn4msFp-CTfsQcB7jdOxYXlPi3kNTTC0BLKlwYgWRKCLHc7gqoghB5~r2vWpWFmSxLjLnC5xk-w3w6IZ9J0eYn24Upkp8mo71e4x1m5WriDO5OnPgNApvnAxN1hU3~TnbLPA6-XbMqrQtDCU-aR0-J1KsM4CIEhb36H~Fr5wCIvxdjJgds5EY5rNTjKeoXI-T0iJPX-CxFtpIYfwgZuh1uC6zKOoVRmXh00F3nssL~UyeeckrdezaQOnN7Gg75S2HQqs1wYd~NgyY2Rf4ZK2FOogbcffcegOT4CeEaRPk9roNjhNezhQ__
+
+
+
+
+    return (
+        <>
+
+            <div className="main">
+                <div className="imgc">
+                    <img className='imghead' src='/image/category.png' alt="Description" />
+                </div>
+                <div className="bannert">
+                    <h2>Categories</h2>
+                    <p>Home-Categories</p>
+                </div>
+
+                <div className="onimage">
+                    <button type='submit' className='btn01'><p>Location <p className='mm01'>Ex Noida</p></p> </button>
+                    <button type='submit' className='btn1'>Services<span class="material-symbols-outlined">
+                        expand_more
+                    </span> </button>
+                    <button type='submit' className='btn1'>Budget<span class="material-symbols-outlined">
+                        expand_more
+                    </span> </button>
+                    <button type='submit' onClick={handleRedirect1} className='btn2' ><span class="material-symbols-outlined">
+                        search
+                    </span>Search</button>
+
+                    {/*  */}
+
+                </div>
+
+
+                <div className="doubleCard">
+
+
+                    {/* <div className="full0"> */}
+
+                    {/*  */}
+
+                    <div className="construction">
+
+                        <div className="mm">
+                            <div className="mm2"></div>
+                            <span className='upper'>Construction</span>
+                        </div>
+                        <div className="cont10">
+                            <div className="photo">
+
+                                <img className='img10' src='https://images.pexels.com/photos/157811/pexels-photo-157811.jpeg' />
+                            </div>
+
+                            <div className="right">
+                                <h4>Shubham Construction Co.</h4>
+                                <p className='sme'>Shubham Construction Co. for your for your next Construction project</p>
+
+                                <div className='t30'>$100 to $200 per square foot</div>
+
+
+                                <div className="loca0">
+                                    <span><i className="fa-solid fa-location-dot"></i>Greater Noida Uttar Pradesh</span>
+
+                                </div>
+
+
+
+                                <div className="uright10">
+                                    <button type='submit' onClick={Mymodel} className='numbtn'> Send Enquiry</button>
+                                    <button type='submit' onClick={handleRedirect} className='numbtn0'>View Details</button>
+                                    <i className="fa-regular fa-heart"></i>
+                                    {payment && <Send band={band} />}
+                                </div>
+
+
+
+
+                            </div>
+
+                        </div>
+
+
+
+
+                    </div>
+
+
+
+
+                    {/*------------------- 2nd------------------------------------------------------------------------------------- */}
+
+
+                    {/* <div className="full1"> */}
+                    {/* <span className='upper'>Construction</span> */}
+
+                    <div className="construction">
+
+                        <div className="mm">
+                            <div className="mm2"></div>
+                            <span className='upper'>Construction</span>
+                        </div>
+                        <div className="cont10">
+                            <div className="photo">
+
+                                <img className='img10' src='https://images.pexels.com/photos/157811/pexels-photo-157811.jpeg' />
+                            </div>
+
+                            <div className="right">
+                                <h4>Shubham Construction Co.</h4>
+                                <p className='sme'>Shubham Construction Co. for your for your next Construction project</p>
+                                <div className='t30'>$100 to $200 per square foot</div>
+
+
+                                <div className="loca0">
+                                    <span><i className="fa-solid fa-location-dot"></i>Greater Noida Uttar Pradesh</span>
+
+                                </div>
+
+
+
+                                <div className="uright10">
+                                    <button type='submit' onClick={Mymodel} className='numbtn'> Send Enquiry</button>
+                                    <button type='submit' onClick={handleRedirect} className='numbtn0'>View Details</button>
+                                    <i className="fa-regular fa-heart"></i>
+                                    {/* {payment && <Send band={band} />}    */}
+                                </div>
+
+
+
+
+                            </div>
+
+                        </div>
+
+
+
+
+                    </div>
+
+
+                </div>
+
+
+                {/* --------------------------second double------------------------------------------------------------------------ */}
+
+                <div className="doubleCard">
+
+
+                    {/* <div className="full0"> */}
+
+                    {/*  */}
+
+                    <div className="construction">
+
+                        <div className="mm">
+                            <div className="mm2"></div>
+                            <span className='upper'>Construction</span>
+                        </div>
+                        <div className="cont10">
+                            <div className="photo">
+
+                                <img className='img10' src='https://images.pexels.com/photos/157811/pexels-photo-157811.jpeg' />
+                            </div>
+
+                            <div className="right">
+                                <h4>Shubham Construction Co.</h4>
+                                <p className='sme'>Shubham Construction Co. for your for your next Construction project</p>
+                                <div className='t30'>$100 to $200 per square foot</div>
+
+
+                                <div className="loca0">
+                                    <span><i className="fa-solid fa-location-dot"></i>Greater Noida Uttar Pradesh</span>
+
+                                </div>
+
+
+
+                                <div className="uright10">
+                                    <button type='submit' onClick={Mymodel} className='numbtn'> Send Enquiry</button>
+                                    <button type='submit' onClick={handleRedirect} className='numbtn0'>View Details</button>
+                                    <i className="fa-regular fa-heart"></i>
+                                    {/* {payment && <Send band={band} />}    */}
+                                </div>
+
+
+
+
+                            </div>
+
+                        </div>
+
+
+
+
+                    </div>
+
+
+
+
+                    {/*------------------- 2nd------------------------------------------------------------------------------------- */}
+
+
+                    {/* <div className="full1"> */}
+                    {/* <span className='upper'>Construction</span> */}
+
+                    <div className="construction">
+
+                        <div className="mm">
+                            <div className="mm2"></div>
+                            <span className='upper'>Construction</span>
+                        </div>
+                        <div className="cont10">
+                            <div className="photo">
+
+                                <img className='img10' src='https://images.pexels.com/photos/157811/pexels-photo-157811.jpeg' />
+                            </div>
+
+                            <div className="right">
+                                <h4>Shubham Construction Co.</h4>
+                                <p className='sme'>Shubham Construction Co. for your for your next Construction project</p>
+                                <div className='t30'>$100 to $200 per square foot</div>
+
+
+                                <div className="loca0">
+                                    <span><i className="fa-solid fa-location-dot"></i>Greater Noida Uttar Pradesh</span>
+
+                                </div>
+
+
+
+                                <div className="uright10">
+                                    <button type='submit' onClick={Mymodel} className='numbtn'> Send Enquiry</button>
+                                    <button type='submit' onClick={handleRedirect} className='numbtn0'>View Details</button>
+                                    <i className="fa-regular fa-heart"></i>
+                                    {/* {payment && <Send band={band} />}    */}
+                                </div>
+
+
+
+
+                            </div>
+
+                        </div>
+
+
+
+
+                    </div>
+                    {/* </div> */}
+
+
+                </div>
+
+
+            </div>
+
+
+
+        </>
+    )
+}
+
+export default Upcategary
